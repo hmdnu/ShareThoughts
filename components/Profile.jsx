121f7d94ce99d25151f11dc9a299d514da6c3ac0
nerots123 <hamdanubaidillah0306@gmail.com>
2023-06-05 14:46:49 +0700

    Fix: Profile: ProfileSize

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,7 +5,7 @@ export default function Profile({ user, isLoading }) {
   return (
     <section className="mt-5">
       <div className="border-[1px] border-black rounded-xl p-5 flex items-center gap-5">
-        {isLoading ? <Spinner width={100} height={100} /> : <Image className="rounded-full" src={user?.image} width={100} height={100} alt="profile pic" />}
+        {isLoading ? <Spinner width={100} height={100} /> : <Image className="rounded-full" src={user?.image} width={80} height={80} alt="profile pic" />}
 
         <div className="flex flex-col gap-1 justify-center items-start w-full">
           <h1 className="font-semibold sm:text-2xl text-xl">{user?.username}</h1>
